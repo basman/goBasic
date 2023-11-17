4f4978c5b776aa36b06588b0d0ea8cf7d805a2f6
Roman Hoog Antink <rha_github@disconnect.ch>
2023-11-17 15:59:03 +0200

    numberGuessing: prepare exercise

---
+++ numberGuessing/main.go
@@ -0,0 +1,11 @@
+package main
+
+func main() {
+	/*
+			TODO Simple Game: Guess the Number
+		        Create a program that generates a random number between 1 and 100.
+		        Ask the user to guess the number.
+		        Provide hints such as "Too high" or "Too low" after each guess.
+		        Continue until the user guesses the correct number.
+	*/
+}
